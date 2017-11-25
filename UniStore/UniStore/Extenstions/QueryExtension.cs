9f1feebddbe4e162a95f3eb21940e6eff80b9da8
Thanh Thinh <thanhthinhcntt@gmail.com>
2017-11-25 16:04:54 +0700

    refactory code

--- UniStore/UniStore/Extenstions/QueryExtension.cs
+++ UniStore/UniStore/Extenstions/QueryExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
@@ -12,6 +13,16 @@ namespace UniStore.Extenstions
         public static IQueryable<T> Including<T>(this IQueryable<T> self, params Expression<Func<T, object>>[] includeProperties) where T : class
         {
             return includeProperties.Aggregate(self, (current, includeProperty) => current.Include(includeProperty));
+        }
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+
+            var attribute
+                = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute))
+                    as DescriptionAttribute;
+
+            return attribute == null ? value.ToString() : attribute.Description;
         }
     }
 }
